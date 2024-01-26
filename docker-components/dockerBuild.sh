1fc66e3c93e9a2b4c52ef452d95f432709bfb4cc
Bartosz Wąsik <wasik.bartosz@outlook.com>
2024-01-26 13:38:49 +0100

    edited the docker files and added an example application.yaml

--- docker-components/dockerBuild.sh
+++ docker-components/dockerBuild.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+cp ../app/target/*.jar .
+
 # Build the Spring Boot application Docker image
 docker build -t baldurs-gate-3-api .
 
 # Run Docker Compose to start the containers
 docker-compose up -d
+
+rm -rf *.jar
